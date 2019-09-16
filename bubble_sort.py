f288194815aa035737b3e715303814faa6f461db
Yana <yanahasson@gmail.com>
2019-09-16 20:31:26 +0200

    Add better bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,3 +7,19 @@ def bubble_sort(nums):
                 nums[idx], nums[higher_idx] = nums[higher_idx], nums[idx]
                 idxs[idx], idxs[higher_idx] = idxs[higher_idx], idxs[idx]
     return nums, idxs
+
+
+def bubble_sort_better(arr):
+    # Each iteration will bring max remaining item to correct position
+    for idx1 in range(len(arr)):
+        # Bubble up until end, then end - 1, ...
+        stop_idx = len(arr) - idx1
+        for idx2 in range(stop_idx - 1):
+            # idx2 + 1 not at target position of max
+            # if left value higher then right position, bubble up left value
+            if arr[idx2] > arr[idx2 + 1]:
+                arr[idx2], arr[idx2 + 1] = arr[idx2 + 1], arr[idx2]
+    return arr
+
+
+print(bubble_sort([-2, 4, 2, 1, 2, 5]))
